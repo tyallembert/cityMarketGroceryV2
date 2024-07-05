ad63bf8b17c2fe48130bca93a755e8fdd8f8dfe7
Ty Allembert <tyallembert@gmail.com>
2024-07-05 10:28:08 -0400

    add: start of admin page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LiveFreightDryGoods from './pages/LiveFreightDryGoods';
+import AdminLiveFreightDryGoods from './pages/AdminLiveFreightDryGoods';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<LiveFreightDryGoods />}/>
+        <Route path='/admin' element={<AdminLiveFreightDryGoods />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/AdminLiveFreightDryGoods.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./AdminLiveFreightDryGoods.scss";
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
+
+const AdminLiveFreightDryGoods = () => {
+  return (
+    <>
+    <Header />
+    <Navigation />
+    <main>
+        Admin Page
+    </main>
+    </>
+  )
+}
+
+export default AdminLiveFreightDryGoods
